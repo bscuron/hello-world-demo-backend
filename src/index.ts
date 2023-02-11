a0392045c56c3a655efd46f6b4b53d1a35a08d79
Ben Scuron <bscuron19@gmail.com>
2023-02-11 18:44:32 -0500

    Add express template

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello, World!");
+// Important external modules
+const express = require('express');
+const cors = require('cors');
+
+// Create express application
+const app = express();
+
+// Allow cross-origin requests
+app.use(cors());
+
+// Parse the incoming requests with JSON payloads
+app.use(express.json());
+
+// Create a GET route
+app.get('/message', (req, res) => {
+    res.json({ message: "Hello, World!!!!!" });
+});
+
+// Start the server, listen for requests on port 8000
+// TODO: create variable for port
+app.listen(8000, () => {
+  console.log(`Server is running on port 8000.`);
+});
