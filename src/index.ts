7f8af39fc189249aa000e09f168c9017572755d6
Benjamin Charles Scuron <bscuron19@gmail.com>
2023-02-17 14:26:49 -0500

    Interface with database using `dotenv` and `mysql`
    `dotenv` used to store credentials in `.env`, the file is not stored
    in the repo for security reasons
    
    `mysql` is used to interface with the database

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+// Load project environment variables (located in `.env`)
+require('dotenv').config();
+
 // Important external modules
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql');
+
+// Connect to the database
+const connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_NAME
+});
+connection.connect(); // TODO: make sure connection succeeded
 
 // Create express application
 const app = express();
@@ -13,7 +26,16 @@ app.use(express.json());
 
 // Create a GET route
 app.get('/message', (req, res) => {
-    res.json({ message: "Hello, World!!!!!" });
+
+    // Query the database
+    connection.query('SELECT * FROM Members', function (error, results, fields) {
+        if (error) throw error;
+        let str = '';
+        for(let row of results) {
+            str += `ID: ${row.id}, FIRST_NAME: ${row.first_name}, LAST_NAME: ${row.last_name}\n`
+        }
+        res.json({ message: str });
+    });
 });
 
 // Start the server, listen for requests on port 8000
